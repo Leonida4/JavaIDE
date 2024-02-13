53e0a5c9f788b639124a4dcbf821b49f7f3f54da
Leonidi4 Car <cat68421@gmail.com>
2024-02-13 09:56:45 +0300

    Новый санин комит

--- Sanya.java
+++ Sanya.java
@@ -1,7 +1,15 @@
 package JavaIDE;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Sanya {
     public static void Play(){
         System.out.println("I am a big BOOB");
+    }
+    public void Gay(){
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
     }
 }
